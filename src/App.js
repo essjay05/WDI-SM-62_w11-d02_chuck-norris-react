de0b52ccf9f9fe404ab19dea70f9ddaaa89741eb
Joy <stepjoyous@gmail.com>
2019-01-23 16:16:46 -0800

    Created visuals for joke app. Structure similar to deliverable.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import Joke from './components/Joke';
+import axios from 'axios';
 
 export default class App extends Component {
 
     state = {
-
+        joke: undefined,
+        favorite: undefined
     }
 
+    
+
     render () {
+        let { joke, favorite } = this.state
         return (
             <main className="container">
-                <h1> Hello World!</h1>
+                <h1> Chuck Norris Jokes </h1>
+                <Joke joke={joke} />
+                <div className="buttons">
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                        type="submit"
+                        value="New Joke"
+                        name="joke" />
+                    </form>
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                        type="submit"
+                        value="Favorite"
+                        name="favorite" />
+                    </form>
+                </div>
             </main>
         )
     }
---
+++ src/components/Joke/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default ({joke}) => {
+    return (
+        <blockquote>{joke}</blockquote>
+    )
+}
